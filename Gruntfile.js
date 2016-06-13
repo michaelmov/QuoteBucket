eecb832c324041648de5d32c8444b916c4e73e6e
Michael Movsesov <michael.movsesov@gmail.com>
2016-06-12 23:35:17 -0700

    Login page UI styles

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,6 +13,21 @@ module.exports = function(grunt) {
             }
         },
 
+        webfont : {
+            icons : {
+                src : 'client/img/icons/*.svg',
+                dest : 'client/css/fonts',
+                destCss: 'client/css/base',
+                options : {
+                    relativeFontPath: 'fonts',
+                    stylesheet : 'scss',
+                    templateOptions : {
+                        classPrefix : 'i-'
+                    }
+                }
+            }
+        },
+
         watch: {
             scss: {
                 files : ['client/css/**/*.scss'],
@@ -23,6 +38,7 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-webfont');
 
 
     grunt.registerTask('compile-css', ['sass']);

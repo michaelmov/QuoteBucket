8ba47fef41d2a338f05baa6519e938517dc6f384
Michael Movsesov <michael.movsesov@gmail.com>
2017-06-01 23:20:49 -0700

    Added firebase config.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAoa2e9fSnnVpYi55-l7U12wEoewdEmi8Y',
+  authDomain: 'quotebucket-627ef.firebaseapp.com',
+  databaseURL: 'https://quotebucket-627ef.firebaseio.com',
+  projectId: 'quotebucket-627ef',
+  storageBucket: 'quotebucket-627ef.appspot.com',
+  messagingSenderId: '283019581956'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
